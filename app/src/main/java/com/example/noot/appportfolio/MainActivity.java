f770f56e7330f88813fac660e78f205e69adb7a0
invapid <invapid@users.noreply.github.com>
2015-07-20 21:32:45 -0400

    tidied MainActivity

--- app/src/main/java/com/example/noot/appportfolio/MainActivity.java
+++ app/src/main/java/com/example/noot/appportfolio/MainActivity.java
@@ -9,16 +9,59 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        /*
-        // OnClickListener method
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void spotify_clicked(View view){
+        Toast.makeText(getApplicationContext(), "launch spotify app", Toast.LENGTH_SHORT).show(); // makeText(Context context, CharSequence text, int duration)
+    }
+    public void scores_clicked(View view){
+        Toast.makeText(getApplicationContext(), "launch scores app", Toast.LENGTH_SHORT).show(); // makeText(Context context, CharSequence text, int duration)
+    }
+    public void library_clicked(View view){
+        Toast.makeText(getApplicationContext(), "launch library app", Toast.LENGTH_SHORT).show(); // makeText(Context context, CharSequence text, int duration)
+    }
+    public void build_clicked(View view){
+        Toast.makeText(getApplicationContext(), "launch build app", Toast.LENGTH_SHORT).show(); // makeText(Context context, CharSequence text, int duration)
+    }
+    public void xyz_clicked(View view){
+        Toast.makeText(getApplicationContext(), "launch xyz app", Toast.LENGTH_SHORT).show(); // makeText(Context context, CharSequence text, int duration)
+    }
+    public void capstone_clicked(View view){
+        Toast.makeText(getApplicationContext(), "launch capstone app", Toast.LENGTH_SHORT).show(); // makeText(Context context, CharSequence text, int duration)
+    }
+}
+
+
+ /*
+// within onCreate -  OnClickListener method
         final Button spotify_app = (Button) findViewById(R.id.spotify_app);
         final Button scores_button = (Button) findViewById(R.id.scores_app);
         final Button library_app = (Button) findViewById(R.id.library_app);
@@ -59,49 +102,3 @@ public class MainActivity extends ActionBarActivity {
             }
         });
         */
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-
-    // button, button2, ... button6
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void spotify_clicked(View view){
-        Toast.makeText(getApplicationContext(), "launch spotify app", Toast.LENGTH_SHORT).show(); // makeText(Context context, CharSequence text, int duration)
-    }
-    public void scores_clicked(View view){
-        Toast.makeText(getApplicationContext(), "launch scores app", Toast.LENGTH_SHORT).show(); // makeText(Context context, CharSequence text, int duration)
-    }
-    public void library_clicked(View view){
-        Toast.makeText(getApplicationContext(), "launch library app", Toast.LENGTH_SHORT).show(); // makeText(Context context, CharSequence text, int duration)
-    }
-    public void build_clicked(View view){
-        Toast.makeText(getApplicationContext(), "launch build app", Toast.LENGTH_SHORT).show(); // makeText(Context context, CharSequence text, int duration)
-    }
-    public void xyz_clicked(View view){
-        Toast.makeText(getApplicationContext(), "launch xyz app", Toast.LENGTH_SHORT).show(); // makeText(Context context, CharSequence text, int duration)
-    }
-    public void capstone_clicked(View view){
-        Toast.makeText(getApplicationContext(), "launch capstone app", Toast.LENGTH_SHORT).show(); // makeText(Context context, CharSequence text, int duration)
-    }
-}
